d966f28455823f6a32f24e1095101282aa594c80
Melvin1perez <125324725+Melvin1perez@users.noreply.github.com>
2023-03-06 21:17:00 -0600

    Add files via upload

---
+++ PARCIAL1-MELVINPEREZ/Negocio/ClsUsuario.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PARCIAL1_MELVINPEREZ.Negocio
+{
+    public class ClsUsuario
+    {
+        public bool Acceso(string nombre, string contra)
+        {
+            // Verificar las creden del usuario
+            return nombre == "TUNOMBRE" && contra == "123";
+        }
+    }
+    public class ClsVenta
+    {
+        public void Cobro(VENTA venta)
+        {
+            decimal total = venta.PrecioDelProducto * venta.CantidadDeProducto;
+
+            // Aplicar descuento si el total de la venta es mayor a $50
+            if (total > 50)
+            {
+                total -= 10;
+                Console.WriteLine($"Precio original: ${venta.PrecioDelProducto * venta.CantidadDeProducto}");
+                Console.WriteLine($"Descuento aplicado: $10");
+                Console.WriteLine($"Total a pagar: ${total}");
+            }
+            else
+            {
+                Console.WriteLine($"Total a pagar: ${total}");
+            }
+        }
+    }
+}
